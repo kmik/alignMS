95988d8967a2f6a9db42827a1f1fa193197f16e8
Mikko Kukkonen <mi.kukkonen@gmail.com>
2021-11-02 20:15:56 +0200

    cleaning up

--- runi_trans5.sh
+++
@@ -1,7 +0,0 @@
-./trans.sh /media/koomikko/LaCie/liperi_2020_photogrammetry/Altum/133_done/ORIG/0011SET/000/aligned/
-./trans.sh /media/koomikko/LaCie/liperi_2020_photogrammetry/Altum/133_done/ORIG/0011SET/000/aligned_2/
-
-./trans.sh /media/koomikko/LaCie/liperi_2020_photogrammetry/Altum/137_done/ORIG/0011SET/000/aligned/
-./trans.sh /media/koomikko/LaCie/liperi_2020_photogrammetry/Altum/137_done/ORIG/0011SET/000/aligned_2/
-
-
--- trans.sh
+++
@@ -1,9 +0,0 @@
-
-
-mkdir -p "$1/tiled"
-
-for f in "$1/"*.tif; do
-	$(basename $a)
-     	gdal_translate "$f" -co TILED=NO "$1tiled/$(basename $f)" -co TILED=YES -co COMPRESS=DEFLATE -co NUM_THREADS=ALL_CPUS
-	bin/Debug/exifTool/exiftool -tagsfromfile "$f" -all:all -xmp "$1tiled/$(basename $f)" -overwrite_original > /dev/null 2>&1
-done
