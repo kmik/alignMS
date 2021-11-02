83aa30bc6b3ce861498dde222fe1cf049a3fa2f6
Mikko Kukkonen <mi.kukkonen@gmail.com>
2021-11-02 20:10:33 +0200

    cleaning up

--- exf.sh
+++
@@ -1,1 +0,0 @@
-echo /media/koomikko/LaCie/tila_2020/Altum/0001SET/000/testaus/%f.%e
--- runi_trans4.sh
+++
@@ -1,6 +0,0 @@
-
-./trans.sh /media/koomikko/LaCie/liperi_2020_photogrammetry/Altum/144_done/ORIG/0013SET/000/aligned/
-./trans.sh /media/koomikko/LaCie/liperi_2020_photogrammetry/Altum/144_done/ORIG/0013SET/000/aligned_2/
-
-
-
