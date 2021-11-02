6284407bb0645b4708c0e68e0ca6f974a4d62333
Mikko Kukkonen <mi.kukkonen@gmail.com>
2021-11-02 20:14:03 +0200

    cleaning up

---
+++ runi_trans4.sh
@@ -0,0 +1,6 @@
+
+./trans.sh /media/koomikko/LaCie/liperi_2020_photogrammetry/Altum/144_done/ORIG/0013SET/000/aligned/
+./trans.sh /media/koomikko/LaCie/liperi_2020_photogrammetry/Altum/144_done/ORIG/0013SET/000/aligned_2/
+
+
+
